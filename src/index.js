b868baf6e85c2f44f5871cda027286a75412d5d9
Bia Ali <biaali201@gmail.com>
2020-11-20 04:06:39 +0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Card from "./Card"
+
+
+ReactDOM.render(
+  <>
+<Card imgsrc= "https://images7.alphacoders.com/760/760394.jpg"
+title = "A Netflix Orignal series "
+sname = " Super Girl" 
+link = "https://www.netflix.com/pk/title/80065386"
+/>
+<Card imgsrc = "https://images3.alphacoders.com/882/882548.jpg"
+title = " A Netflix Orignal series"
+sname = "Stranger Things" 
+link = "https://www.netflix.com/pk/title/80057281" />
+<Card imgsrc ="https://images5.alphacoders.com/856/856344.jpg"
+title = "A Netflix Orignal series "
+sname = "The Flash" 
+link = "https://www.netflix.com/pk/title/80027042" />
+<Card imgsrc ="https://images6.alphacoders.com/334/334230.jpg"
+title = "A Netflix Orignal series "
+sname = "Arrow" 
+link = "https://www.netflix.com/pk/title/70242081" />
+<Card imgsrc ="https://wallpapercave.com/wp/wp5217306.png"
+title = "A Netflix Orignal series "
+sname = "Resurrection: Ertugrul" 
+link = "https://www.netflix.com/pk/title/80127001" />
+<Card imgsrc ="https://images6.alphacoders.com/299/299424.jpg"
+title = "A Netflix Orignal series "
+sname = "Secret of the Nile" 
+link = "https://www.netflix.com/pk/title/80215154" />
+</>
+
+,document.getElementById('root')
+);
+
+
